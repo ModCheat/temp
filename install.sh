71cc12638f2eb65d42c716d1b53aefbf93cee19f
lover3418 <lover3418@gmail.com>
2019-12-07 19:52:15 +0530

    U

--- install.sh
+++ install.sh
@@ -14,29 +14,12 @@ echo ""
 echo "Installation of SparkVVIP AntiBAN"
 echo "--0%"
 mkdir /storage/emulated/0/.ori
-cp -r /data/data/com.tencent.ig/lib/libgcloud.so /storage/emulated/0/.ori
-cp -r /data/data/com.tencent.ig/lib/libTDataMaster.so /storage/emulated/0/.ori
-cp -r /data/data/com.tencent.ig/lib/libIMSDK.so /storage/emulated/0/.ori
-cp -r /data/data/com.tencent.ig/lib/libUE4.so /storage/emulated/0/.ori
-cp -r /data/data/com.tencent.ig/lib/libtersafe.so /storage/emulated/0/.ori
-cp -r /data/data/com.tencent.ig/lib/libBugly.so /storage/emulated/0/.ori
+cp -r /data/data/com.pubg.krmobile/lib/libUE4.so /storage/emulated/0/.ori
+cp -r /data/data/com.pubg.krmobile/lib/libtersafe.so /storage/emulated/0/.ori
+
 
 echo "---------25%"
-sleep 2
-cp -r /data/data/com.tencent.ig/lib/libUE4.so /storage/emulated/0/.ori
+
 echo "--------------50%"
-sleep 2
-cp -r /data/data/com.tencent.ig/files/tss_tmp/comm.dat /storage/emulated/0/.ori
-cp -r /data/data/com.tencent.ig/files/tss_tmp/config3.xml /storage/emulated/0/.ori
-cp -r /data/data/com.tencent.ig/files/tss_tmp/gp4.ano.dat /storage/emulated/0/.ori
-cp -r /data/data/com.tencent.ig/files/tss_tmp/mn_cache.dat /storage/emulated/0/.ori
-cp -r /data/data/com.tencent.ig/files/tss_tmp/mrpcs.data /storage/emulated/0/.ori
-cp -r /data/data/com.tencent.ig/files/tss_tmp/tss.ano.dat /storage/emulated/0/.ori
-echo "----------------------75%"
-sleep 2
-cp -r /data/data/com.tencent.ig/files/tss_tmp/tss_base.dat /storage/emulated/0/.ori
-cp -r /data/data/com.tencent.ig/files/tss_tmp/tss_cfg2.dat /storage/emulated/0/.ori
-cp -r /data/data/com.tencent.ig/files/tss_tmp/tss_emu_c2.dat /storage/emulated/0/.ori
-cp -r /data/data/com.tencent.ig/files/tss_tmp/tssmua.zip /storage/emulated/0/.ori
 echo "-------------------------------------100%"
 echo "Installation Successful...😎😎😎"
